25dc0e5291c9bf86b6042eae1a6c3352b49b6028
hobs <hobs+github@totalgood.com>
2019-08-05 10:58:46 -0700

    about route

--- unredactor/app/routes.py
+++ unredactor/app/routes.py
@@ -2,8 +2,19 @@ from flask import request
 from flask import render_template
 from app import app
 
+
+def sort_words(text):
+    return ' '.join(sorted(text.split()))
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-	text = request.args.get('text')
-	return render_template('unredacted.json', text=text)
+    text = request.args.get('text')
+    unredacted_text = sort_words(text)
+    return render_template('index.html', text=text, unredacted_text=unredacted_text)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', **locals())
