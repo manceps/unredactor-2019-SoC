30e274c4297ed0f489cf5c1ad7d38d5e0cadc936
Julian McOmie <julian.mcomie@gmail.com>
2019-08-08 00:55:53 -0700

    added unredact button + improved validators

--- unredactor/app/forms.py
+++ unredactor/app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class UnredactForm(FlaskForm):
-	text = TextAreaField('Text')
-
+	text = TextAreaField('Text', validators=[DataRequired()])
+	submit = SubmitField('Unredact')
--- unredactor/app/routes.py
+++ unredactor/app/routes.py
@@ -1,5 +1,5 @@
 from flask import request
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import UnredactForm
 
@@ -24,7 +24,10 @@ def unredact():
     unredacted_text = sort_words(text)
     return render_template('unredacted_text.json', text=text, unredacted_text=unredacted_text)
 
-@app.route('/unredactor')
+@app.route('/unredactor', methods=['GET', 'POST'])
 def unredactor():
 	form = UnredactForm()
+	if form.validate_on_submit():
+		flash('Unredact request')
+		return redirect('/unredactor')
 	return render_template('unredact.html', title='Unredact', form=form)
