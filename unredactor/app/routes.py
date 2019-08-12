19d2e77c2a9c91479dd6b149c606569590944b48
Julian McOmie <julian.mcomie@gmail.com>
2019-08-11 19:20:37 -0700

    add about page to navigation bar, renamed about3.html to about.html

--- unredactor/app/routes.py
+++ unredactor/app/routes.py
@@ -7,10 +7,6 @@ from app.forms import UnredactForm
 #from muellerbot import unredact
 from unredactor_functions import unredact
 
-# def sort_words(text):
-#     return ' '.join(sorted(text.split()))
-
-
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,7 +16,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', **context)
-
 
 @app.route('/api')
 def api():
